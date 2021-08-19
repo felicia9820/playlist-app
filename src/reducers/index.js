d765fb1fb86feece71aa1beb5fe1f51b837f3338
felicia9820 <feliciaroman2@gmail.com>
2021-08-19 12:11:49 +0200

    init commit

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux';
+
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4:05' },
+        { title: 'Blank Space', duration: '3:24' },
+        { title: 'Almost Love', duration: '2:36' },
+        { title: 'Toxic', duration: '5:12' }
+    ];
+};
+
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
